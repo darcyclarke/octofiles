d167b85441a0d1e7e4b9bfcc8d17aaf60cb8dfd8
Darcy Clarke <darcy@darcyclarke.me>
2019-06-03 16:00:41 -0400

    Change directory structure to make building/distrobution to marketplaces

--- scripts/set-manifest-version.js
+++ scripts/set-manifest-version.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const package = require('../package')
-const manifest = require('../manifest')
+const manifest = require('../src/manifest')
 console.log('Updating version:', manifest.version, 'to', package.version)
 manifest.version = package.version
-fs.writeFileSync(path.resolve(__dirname, '../manifest.json'), JSON.stringify(manifest, null, 2))
+fs.writeFileSync(path.resolve(__dirname, '../src/manifest.json'), JSON.stringify(manifest, null, 2))
